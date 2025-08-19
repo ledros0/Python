dc1257551d65b3e4f8b28950b3cae8468a64ee3e
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-08-19 04:00:05 -0300

    Update Conversor.py

--- inicio de python/Conversor.py
+++ inicio de python/Conversor.py
@@ -1,5 +1,15 @@
-def ConverteMetros(metros):
+def ConverteMetrosParaPes(metros):
     return metros * 3.281;
-    
-def ConvertePes(pes):
-    return pes * 0.3048
+def ConverteMetrosParaJardas(metros):
+    return metros * 1.094;
+
+def ConvertePesParaMetros(pes):
+    return pes * 0.3048;
+def ConvertePesParaJardas(pes):
+    return pes * 0.333;
+
+def ConverteJardaParaMetros(jardas):
+    return jardas * 0.9144;
+def ConverteJardaParaPes(jardas):
+    return jardas * 3;
+
