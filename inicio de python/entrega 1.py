174ee7c74cb8a0b9cc91bd5e0fb1a12912cfe977
Mateus Borba <130244552+ledros0@users.noreply.github.com>
2025-08-19 03:58:56 -0300

    Update entrega 1.py

--- inicio de python/entrega 1.py
+++ inicio de python/entrega 1.py
@@ -1,15 +1,41 @@
 # coding: utf-8 
 import Conversor 
 
-escolha = input("O que voce quer converter? \n1 - Metros \n2 - Pes\n\n");
+escolha = input("O que voce quer converter? \n1 - Metros \n2 - Pes\n3 - Jardas\n");
 
 if(escolha == "1"):
     metros = input("Digite a quantidade de metros\n");
-    metrosConvertidos = Conversor.ConverteMetros(float(metros));
-    print(f"Os {metros} metros viraram {metrosConvertidos:.2f} pes!");
+    escolha2 = input("Para o que voce quer converter?\n1 - Pes\n2 - Jardas\n")
+
+    if(escolha2 == "1"):
+       metrosConvertidos = Conversor.ConverteMetrosParaPes(float(metros));
+       print(f"Os {metros} metros viraram {metrosConvertidos:.2f} pes!");
+
+    elif(escolha2 == "2"):
+        metrosConvertidos = Conversor.ConverteMetrosParaJardas(float(metros));
+        print(f"Os {metros} metros viraram {metrosConvertidos:.2f} jardas!");
     
+
 elif(escolha == "2"):
     pes = input("Digite a quantidade de pes\n");
-    pesConvertidos = Conversor.ConvertePes(float(pes));
-    print(f"Os {pes} pes viraram {pesConvertidos:.2f} metros!");
+    escolha2 = input("Para o que voce quer converter?\n1 - Metros\n2 - Jardas\n");
 
+    if(escolha2 == "1"):
+      pesConvertidos = Conversor.ConvertePesParaMetros(float(pes));
+      print(f"Os {pes} pes viraram {pesConvertidos:.2f} metros!");
+
+    elif(escolha2 == "2"):
+      pesConvertidos = Conversor.ConvertePesParaJardas(float(pes));
+      print(f"Os {pes} pes viraram {pesConvertidos:.2f} jardas!");
+
+elif(escolha == "3"):
+    jardas = input("Digite a quantidade de jardas\n");
+    escolha2 = input("Para o que voce quer converter?\n1 - Metros\n2 - Pes\n");
+
+    if(escolha2 == "1"):
+      pesConvertidos = Conversor.ConverteJardaParaMetros(float(jardas));
+      print(f"As {jardas} pes viraram {pesConvertidos:.2f} metros!");
+
+    elif(escolha2 == "2"):
+      pesConvertidos = Conversor.ConverteJardasParaPes(float(jardas));
+      print(f"As {jardas} pes viraram {pesConvertidos:.2f} pes!");
